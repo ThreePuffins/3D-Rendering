9dfdfbe5538a89934b9a58dce779efbf04e36807
ThreePuffins <Tristan.Randall4@education.nsw.gov.au>
2026-07-04 13:38:24 +1000

    Fixed triangle rendering ranges and something else probably (it started to just work)

--- 3D_Renderer/main.cpp
+++ 3D_Renderer/main.cpp
@@ -4,8 +4,8 @@
 #include "geometry.h"
 #include "model.h"
 
-constexpr int width  = 200;
-constexpr int height = 200;
+constexpr int width  = 1024;
+constexpr int height = 1024;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -23,14 +23,15 @@ void perspective(const double f) {
     Perspective = {{{1,0,0,0},{0, 1, 0, 0},{0,0,1,0},{0,0,-1/f,1}}};
 }
 
+
 void lookat(const vec3 eye, const vec3 center, const vec3 up) {
     //computes basis vectors for the model from the camera's view
     vec3 n = normalised(eye-center);
     vec3 l = normalised(cross(up,n));
-    vec3 m = normalised(cross(n,l));
+    vec3 m = normalised(cross(n, l));
     //model view converts coordinates from origin O to origin C (the center)
-    ModelView = matrix<4,4,double>{{{l.x,l.y,l.z,0},{m.x,m.y,m.z,0},{n.x,n.y,n.z,0},{0,0,0,1}}} *
-                matrix<4,4,double>{{{1,0,0,-center.x},{0,1,0,-center.y},{0,0,1,-center.z},{0,0,0,1}}};
+    ModelView = matrix<4,4,double>{{{l.x,l.y,l.z,0}, {m.x,m.y,m.z,0}, {n.x,n.y,n.z,0}, {0,0,0,1}}} *
+                matrix<4,4,double>{{{1,0,0,-center.x}, {0,1,0,-center.y}, {0,0,1,-center.z}, {0,0,0,1}}};
 }
 
 vec3 rot(vec3 v, double x, double y, double z) {
@@ -49,8 +50,8 @@ void drawTriangle(const vec4 clip[3], TGAImage &framebuffer, TGAColor col, std::
     auto [box_xmin,box_xmax] = std::minmax({screen[0].x,screen[1].x,screen[2].x});
     auto [box_ymin,box_ymax] = std::minmax({screen[0].y,screen[1].y,screen[2].y});
 #pragma omp parallel for
-    for (int x=std::max<int>(box_xmin,0); x<std::min<int>(box_xmax,framebuffer.width()-1); x++) {
-        for (int y=std::max<int>(box_ymin,0); y<std::min<int>(box_ymax,framebuffer.height()-1); y++) {
+    for (int x=std::max<int>(box_xmin,0); x<=std::min<int>(box_xmax,framebuffer.width()-1); x++) {
+        for (int y=std::max<int>(box_ymin,0); y<=std::min<int>(box_ymax,framebuffer.height()-1); y++) {
             //https://haqr.eu/tinyrenderer/barycentric/ for barycentric coordinate calculation
             vec3 bc = ABC.invertTransposed() * vec3{(double)x,(double)y,1.};
             if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue;
@@ -68,7 +69,7 @@ int main(int argc, char** argv) {
         return 1;
     } 
 
-    vec3 eye {-2,0,0};
+    vec3 eye {-1,0,2};
     vec3 center {0,0,0};
     vec3 up {0,1,0};
     lookat(eye, center, up);
@@ -77,35 +78,19 @@ int main(int argc, char** argv) {
 
     TGAImage framebuffer(width, height, TGAImage::RGB);
     std::vector<double> zbuffer(width*height,-std::numeric_limits<double>::max());
-
-    // Model model = Model(argv[1]);
-    // for (int i = 0; i < model.numFaces(); i++) {
-    //     vec4 clip[3];
-    //     for (int a : {0,1,2}) {
-    //         vec3 v = model.vert(i, a);
-    //         clip[a] = Perspective * ModelView * vec4{v.x,v.y,v.z,1.};
-    //     }
-
-    //     TGAColor rnd;
-    //     for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
-    //     drawTriangle(clip, framebuffer, rnd, zbuffer);
-    // }
-
-
     vec4 clip[3];
-    clip[0] = Perspective * (ModelView * vec4{0,1,0,1.});
-    clip[1] = Perspective * (ModelView * vec4{-0.5,0.5,0,1.});
-    clip[2] = Perspective * (ModelView * vec4{0.5,-0.5,0,1.});
+    Model model = Model(argv[1]);
+    for (int i = 0; i < model.numFaces(); i++) {
+        vec4 clip[3];
+        for (int a : {0,1,2}) {
+            vec3 v = model.vert(i, a);
+            clip[a] = Perspective * ModelView * vec4{v.x,v.y,v.z,1.};
+        }
 
-
-    for(int i=0;i<3;i++)
-        for(int j=0;j<4;j++)
-            std::cerr << "vert: " << i << " axis: " << j << " value: "<< clip[i][j] << std::endl;
-    
-    TGAColor rnd;
-    for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
-    drawTriangle(clip, framebuffer, rnd, zbuffer);
-
+        TGAColor rnd;
+        for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
+        drawTriangle(clip, framebuffer, rnd, zbuffer);
+    }
 
     TGAImage depthbuffer(width, height, TGAImage::GRAYSCALE);
     for (int x = 0; x < width; x++)
@@ -113,7 +98,6 @@ int main(int argc, char** argv) {
             depthbuffer.set(x, y, {(unsigned char)zbuffer[x+width*y]});
 
     
-            
     depthbuffer.write_tga_file("depthbuffer.tga");
     
     framebuffer.write_tga_file("framebuffer.tga");
--- 3D_Renderer/test.cpp
+++ 3D_Renderer/test.cpp
@@ -3,20 +3,19 @@
 
 int main(int argc, char** argv) {
 
-    vec3 a = {4,-2,1};
+    vec3 a = {2,3,4};
+    vec3 b = {5,6,7};
+    vec3 c = cross(a,b);
 
-    matrix<2,2, double> mat = {{{4,7},{2,6}}};
-
-    matrix<2,2, double> m1 = mat.invert();    
-
-    for (int i=0;i<2;i++)
-        for (int j=0;j<2;j++)
-            std::cerr << m1[i][j] << std::endl;
+    for (int i=0;i<3;i++) {
+        std::cerr << c[i] << std::endl;
+    }
 }
 
 // checked off geometry.h:
 // matrix * matrix multiplication
 // determinant
-// cross product
+// cross product (checked it twice)
 // normalisation/norm
 // vector matrix multiplication is fine
+// IT WAS THE COFACTOR FUUUUUHHH
